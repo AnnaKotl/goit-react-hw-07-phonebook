ce350f3245f96e3e99f84b7facb35264421c88db
Анна Котляр <kotlyaranya1771@gmail.com>
2023-08-26 11:26:16 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,9 @@ import { PageContainer, Heading, Section, SubHeading } from './components/styles
 import { EmptyContactsMessage } from './components/EmptyContactsMessage';
 
 export const App = () => {
-  const initialContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-
   const [contacts, setContacts] = useState(() => {
     const savedContacts = localStorage.getItem('contacts');
-    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+    return savedContacts ? JSON.parse(savedContacts) : [];
   });
 
   const [filter, setFilter] = useState('');
