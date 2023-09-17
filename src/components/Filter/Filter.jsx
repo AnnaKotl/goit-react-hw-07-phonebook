ad1f7475cc083154d4ba4b1dbb014886d8ed8d53
Анна Котляр <kotlyaranya1771@gmail.com>
2023-09-17 14:22:09 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter } from 'components/redux/filterSlice';
 import { FilterInput } from './Filter.styled';
 
-export const Filter = ({ value, onChange }) => (
-  <FilterInput
-    type="text"
-    value={value}
-    onChange={onChange}
-    placeholder="Filter contacts by name"
-  />
-);
+export const Filter = () => {
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const handleFilterChange = e => {
+    dispatch(setFilter(e.target.value));
+  };
+
+  return (
+    <FilterInput
+      type="text"
+      value={filter}
+      onChange={handleFilterChange}
+      placeholder="Filter contacts by name"
+    />
+  );
+};
+
+// import React from 'react';
+// import { FilterInput } from './Filter.styled';
+
+// export const Filter = ({ value, onChange }) => (
+//   <FilterInput
+//     type="text"
+//     value={value}
+//     onChange={onChange}
+//     placeholder="Filter contacts by name"
+//   />
+// );
